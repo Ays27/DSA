f451d9d92f7302fe03ef286e10b54199e0a2c551
Aynalem Sitotaw <85031945+Ays27@users.noreply.github.com>
2026-05-28 18:52:28 +0300

    Time: 1413 ms (100.00%) | Memory: 201.6 MB (100.00%) - LeetSync

---
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.ts
@@ -0,0 +1,60 @@
+function stringIndices(wordsContainer: string[], wordsQuery: string[]): number[] {
+
+    class TrieNode {
+        children: Map<string, TrieNode> = new Map();
+        idx: number = -1;
+    }
+
+    const root = new TrieNode();
+
+    // Update best index for a node
+    function update(node: TrieNode, i: number) {
+        if (
+            node.idx === -1 ||
+            wordsContainer[i].length < wordsContainer[node.idx].length ||
+            (
+                wordsContainer[i].length === wordsContainer[node.idx].length &&
+                i < node.idx
+            )
+        ) {
+            node.idx = i;
+        }
+    }
+
+    // Build reversed trie
+    for (let i = 0; i < wordsContainer.length; i++) {
+        let node = root;
+        update(node, i);
+
+        const word = wordsContainer[i];
+
+        for (let j = word.length - 1; j >= 0; j--) {
+            const ch = word[j];
+
+            if (!node.children.has(ch)) {
+                node.children.set(ch, new TrieNode());
+            }
+
+            node = node.children.get(ch)!;
+            update(node, i);
+        }
+    }
+
+    const ans: number[] = [];
+
+    // Query
+    for (const word of wordsQuery) {
+        let node = root;
+
+        for (let j = word.length - 1; j >= 0; j--) {
+            const ch = word[j];
+
+            if (!node.children.has(ch)) break;
+            node = node.children.get(ch)!;
+        }
+
+        ans.push(node.idx);
+    }
+
+    return ans;
+}

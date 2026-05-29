65bb6bb3e0885985520bef0281accbaa8b9db216
Aynalem Sitotaw <85031945+Ays27@users.noreply.github.com>
2026-05-29 20:03:51 +0300

    Time: 1 ms (100.00%) | Memory: 58.1 MB (79.55%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.ts
@@ -0,0 +1,16 @@
+function minElement(nums: number[]): number {
+    let min = Infinity;
+
+    for (let num of nums) {
+        let sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+
+        min = Math.min(min, sum);
+    }
+
+    return min;
+}
